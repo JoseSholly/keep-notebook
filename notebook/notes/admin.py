ae4489401cc21da3ca677d3bc284b2d79d807eda
JoseSholly <josepholawuyi30@gmail.com>
2024-07-09 20:01:53 +0100

    Add Note model

--- notebook/notes/admin.py
+++ notebook/notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Note
 # Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display= ['id', 'title', 'body', 'archived', 'pinned', 'created', 'updated']
+    search_fields= ['title', 'body', 'updated']
+    list_filter = ['pinned', 'created', 'updated']
--- notebook/notes/models.py
+++ notebook/notes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    title= models.CharField(max_length=100, null=False)
+    body= models.TextField()
+    archived= models.BooleanField(default=False)
+    pinned= models.BooleanField(default= False)
+    created = models.DateTimeField(auto_now_add= True)
+    updated= models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering= ['-updated']
+        indexes= [
+            models.Index(fields=["-updated"])
+        ]
+        verbose_name = "Note"
+        verbose_name_plural= "Notes"
+
+    def __str__(self) -> str:
+        return self.title
+
+    
