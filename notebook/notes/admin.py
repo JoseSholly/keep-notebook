09fa5bcba51007c1e4091e058dd064ff80665a6a
JoseSholly <josepholawuyi30@gmail.com>
2024-07-11 20:57:48 +0100

    Label field update

--- notebook/notes/admin.py
+++ notebook/notes/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Label
 # Register your models here.
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display= ['id', 'title', 'body', 'archived', 'pinned', 'created', 'updated']
-    search_fields= ['title', 'body', 'updated']
+    list_display= ['id', 'title','label','created', 'updated']
+    search_fields= ['title', 'body', 'updated', 'archived', 'pinned', ]
     list_filter = ['pinned', 'created', 'updated']
+
+class NoteInline(admin.TabularInline):
+    model= Note
+    extra=0
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = [ 'name']
+    search_fields = ['name']
+    inlines = [NoteInline]
--- notebook/notes/models.py
+++ notebook/notes/models.py
@@ -3,9 +3,21 @@ from django.db import models
 # Create your models here.
 
 
+class Label(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    class Meta:
+        verbose_name = "Label"
+        verbose_name_plural= "Labels"
+
+
+    def __str__(self):
+        return self.name
+
 class Note(models.Model):
     title= models.CharField(max_length=100, null=False)
     body= models.TextField()
+    label= models.ForeignKey(Label, on_delete=models.SET_NULL, related_name="notes", blank=True, null=True)
     archived= models.BooleanField(default=False)
     pinned= models.BooleanField(default= False)
     created = models.DateTimeField(auto_now_add= True)
@@ -22,5 +34,3 @@ class Note(models.Model):
 
     def __str__(self) -> str:
         return self.title
-
-    
