1e99aba8cf9567930436f156a0ddcd8c54f6f109
JoseSholly <josepholawuyi30@gmail.com>
2024-08-02 22:38:05 +0100

    Styling login page

--- notebook/accounts/forms.py
+++ notebook/accounts/forms.py
@@ -16,7 +16,9 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(label='Email', max_length=254)
+    username = forms.EmailField(label='Email', max_length=254, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'form2Example1'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'form2Example2'}))
+    
 
 
 class CustomPasswordResetForm(PasswordResetForm):
