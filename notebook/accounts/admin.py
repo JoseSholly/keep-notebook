e2924d1c3e664a4058f14c13e3a3a355bfce43d0
JoseSholly <josepholawuyi30@gmail.com>
2024-08-02 23:15:00 +0100

    Add note to user admin panel

--- notebook/accounts/admin.py
+++ notebook/accounts/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from notes.models import Note  
 
+class NoteInline(admin.TabularInline):  # You can also use admin.StackedInline for a different layout
+    model = Note
+    extra = 0  # Number of empty forms to display
+    fields = ['title', 'body', 'label', 'archived', 'pinned', 'created', 'updated']
+    readonly_fields = ['created', 'updated']
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
@@ -12,7 +18,6 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-
     )
     add_fieldsets = (
         (None, {
@@ -22,5 +27,6 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ('email',)
     ordering = ('email',)
+    inlines = [NoteInline]  # Add this line to include the NoteInline
 
 admin.site.register(CustomUser, CustomUserAdmin)
