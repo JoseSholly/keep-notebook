5ab03e0c06e20f1de8e3fd23252ea0127e78c4a5
JoseSholly <josepholawuyi30@gmail.com>
2024-08-28 23:53:15 +0100

    admin action: trash and restore note

--- notebook/notes/admin.py
+++ notebook/notes/admin.py
@@ -1,19 +1,33 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Note, Label
-# Register your models here.
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display= ['id', 'user','title','label','created', 'updated']
-    search_fields= ['user','title', 'body', 'updated', 'archived', 'pinned', ]
-    list_filter = ['pinned', 'created', 'updated', 'archived', 'trashed',]
+    list_display = ['id', 'user', 'title', 'label', 'created', 'updated', 'is_trashed']
+    search_fields = ['user', 'title', 'body', 'updated', 'archived', 'pinned']
+    list_filter = ['pinned', 'created', 'updated', 'archived', 'trashed']
+    actions = ['move_to_trash', 'restore_from_trash']
+
+    def is_trashed(self, obj):
+        return obj.trashed
+    is_trashed.boolean = True
+    is_trashed.short_description = 'Trashed'
+
+    @admin.action(description='Trash selected notes')
+    def move_to_trash(self, request, queryset):
+        queryset.update(trashed=True)
+
+    @admin.action(description='Restore selected notes')
+    def restore_from_trash(self, request, queryset):
+        queryset.update(trashed=False)
 
 class NoteInline(admin.TabularInline):
-    model= Note
-    extra=0
+    model = Note
+    extra = 0
 
 @admin.register(Label)
 class LabelAdmin(admin.ModelAdmin):
-    list_display = [ 'name']
+    list_display = ['name']
     search_fields = ['name']
     inlines = [NoteInline]
